1cebd489cd4e3b8af5ab38fc514dad8766c02b92
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2023-09-27 21:31:41 +0530

    Create PARPER.py

---
+++ starters 102 div 2/PARPER.py
@@ -0,0 +1,122 @@
+from math import *
+import copy
+import itertools
+import bisect
+import sys
+from heapq import heapify, heappop, heappush
+from collections import deque
+
+import os
+
+import sys
+
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+# input = lambda: sys.stdin.readline().rstrip("\r\n")
+# sys.setrecursionlimit(3*10**5)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def  countSetBits(n):
+    count = 0
+    while (n):
+        count += n & 1
+        n >>= 1
+    return count
+
+def ilst():
+    return list(map(int,input().split()))
+    
+def islst():
+    return list(map(str,input().split()))
+    
+def inum():
+    return map(int,input().split())
+    
+def freq(l):
+    d = {}
+    for i in l:
+        d[i] = d.get(i,0)+1
+    return d
+
+def log(n, k):
+    count = 0
+    while n > 1 :
+        count+= 1 
+        n = n//k
+    return count
+    
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+mod = 10**9+7
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    count_odd = 0
+    count_even = 0
+    for i in arr:
+            if i % 2 == 0:
+                count_even += 1 
+            else:
+                count_odd += 1
+    if n == 1:
+        print(1)
+    elif k == 0 and n %2 == 1:
+        if count_even == n or count_odd == n:
+            print(factorial(n)%mod)
+        else:
+            print(0)
+    elif k == 1 and n%2 == 1:
+        if abs(count_odd-count_even) == 1:
+            print((factorial(count_even)*factorial(count_odd))%mod)
+        else:
+            print(0)
+    elif k == 0 and n%2 == 0:
+        if count_even == n or count_odd == n:
+            print(factorial(n)%mod)
+        else:
+            print(0)
+    else:
+        if count_even == count_odd:
+            print((factorial(count_even)*factorial(count_odd)*2)%mod)
+        else:
+            print(0)
